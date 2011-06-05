25f71f386b711e3af4c75e6837cb3b0ca4517ce8
Tim Carey-Smith <tim@spork.in>
2011-06-04 19:44:09 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mastery::Application.config.secret_token = '282b17aee7d1de75b9ded6c693663d6bf5d4dfde0c33474998d73d708bd01a2e2b7e4c1be559a950f7f53fd6f8da2a9bc108234fe740a2ca282f99fa3e3483c5'
