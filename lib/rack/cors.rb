0bacd3b4efc30e0f6868177bf06d553221b3a932
Tim Carey-Smith <tim@spork.in>
2011-06-08 19:37:17 -0700

    Add Rack::Cors to support cross site requests

---
+++ lib/rack/cors.rb
@@ -0,0 +1,25 @@
+module Rack
+  class Cors
+    def initialize(app)
+      @app = app
+    end
+
+    def call(env)
+      if env["REQUEST_METHOD"] == "OPTIONS"
+        headers = {
+          "Content-Type" => "text/plain",
+          "Access-Control-Allow-Origin" => "*",
+          "Access-Control-Allow-Methods" => "GET, PUT",
+          "Access-Control-Allow-Headers" => env["HTTP_ACCESS_CONTROL_REQUEST_HEADERS"],
+        }
+        [200, headers, [""]]
+      else
+        status, headers, body = @app.call(env)
+        if origin = env["HTTP_ORIGIN"]
+          headers["Access-Control-Allow-Origin"] = origin
+        end
+        [status, headers, body]
+      end
+    end
+  end
+end
